4bd64c15eddc1de0a8717b19c6337d81942b266f
Corban <sonnemanc@gmail.com>
2022-04-27 12:43:20 -0700

    added explanation

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -29,4 +29,12 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+
+  //declare an array with the first two values
+  //set a counter equal to the starting length of the array
+  //set a while loop to iterate until the count is no longer smaller than the provided num
+  //in loop the last two values in the array will be summed and that sum will be added to the end of the array.
+  //before the next iteration the counter is shifted up one increment until it reaches the provided number.
+
+
 // And a written explanation of your solution
